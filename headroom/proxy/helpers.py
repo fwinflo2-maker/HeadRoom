608c6a15d86e426f382d11ef529a88301c3af1c8
Tejas Chopra <chopratejas@gmail.com>
2026-07-12 09:17:27 -0700

    style: sort imports after merge (ruff)

--- headroom/proxy/helpers.py
+++ headroom/proxy/helpers.py
@@ -24,8 +24,8 @@ from typing import TYPE_CHECKING, Any, Literal, cast
 
 from headroom import paths as _paths
 from headroom._subprocess import run
-from headroom.proxy import diagnostic_decode_policy, request_limit_policy, wire_debug_redaction_policy
 from headroom.proxy import (
+    diagnostic_decode_policy,
     request_limit_policy,
     sse_byte_buffer_policy,
     wire_debug_format_policy,
